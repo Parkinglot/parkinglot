1a70a92f142785e28f9c9582549f01f91d9740cb
Shailpik <shailpikroy@gmail.com>
2015-02-28 16:06:35 -0600

    hi

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,7 +5,7 @@ angular.module('starter.controllers', [])
 {
   //start map
   $scope.initialise = function() {
-    var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+    var myLatlng = new google.maps.LatLng(40.113803,-88.2249157,17);
     var mapOptions = {
       center: myLatlng,
       zoom: 16,
@@ -13,14 +13,30 @@ angular.module('starter.controllers', [])
     };
     
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-    //Add markers
+    var beaches = [
+      [ 40.890542, -88.274856, 4],
+      [ 40.923036, -88.259052, 5],
+      [ 40.028249, -88.157507, 3],
+      [40.80010128657071, -88.28747820854187, 2],
+      [40.950198, -88.259302, 1]
+    ];
     var icon = {
             url: "img/profile.jpg", //url
             scaledSize: new google.maps.Size(26, 26), //size
             origin: new google.maps.Point(0,0), //origin
             anchor: new google.maps.Point(0,29) //anchor 
     };
+    for (var i = 0; i < beaches.length; i++) 
+    {
+      var beach = beaches[i];
+      var myLatLng = new google.maps.LatLng(beach[0], beach[1]);
+      var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          icon: icon,
+          zIndex: beach[2]
+      });
+    }
     var backicon = {
             url: "img/pin.png", //url
             scaledSize: new google.maps.Size(50, 60), //size
@@ -28,6 +44,7 @@ angular.module('starter.controllers', [])
             anchor: new google.maps.Point(12,28) //anchor 
     };
 
+    //Add markers
     //set current position
     navigator.geolocation.getCurrentPosition(function(pos) {
         map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
