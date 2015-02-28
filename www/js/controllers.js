4b571db1e0d162f64002b74c3bbfacbcb5726c9c
Shailpik <shailpikroy@gmail.com>
2015-02-27 19:27:05 -0600

    changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -26,3 +26,15 @@ angular.module('starter.controllers', [])
     enableFriends: true
   };
 });
+$scope.fbLogin = function() {
+    openFB.login(
+        function(response) {
+            if (response.status === 'connected') {
+                console.log('Facebook login succeeded');
+                $scope.closeLogin();
+            } else {
+                alert('Facebook login failed');
+            }
+        },
+        {scope: 'email,publish_actions'});
+}
